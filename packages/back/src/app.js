1817e77aa7ba74bb4ec8c7463544c476b7be8fc3
Xgt72 <xavier.georget@wildcodeschool.fr>
2019-09-18 20:56:17 +0200

    precommit config

--- packages/back/src/app.js
+++ packages/back/src/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
-// import bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.json({ foo: 'bar' }));
 
