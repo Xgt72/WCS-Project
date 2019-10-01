42004afa3a7f99e6e9c758d5c819d0419780ef7e
Xgt72 <xavier.georget@wildcodeschool.fr>
2019-10-01 08:53:54 +0200

    remove of dist folder and add it in .gitignore

--- packages/back/dist/BaseResponse.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BaseResponse {
-}
-exports.BaseResponse = BaseResponse;
-//# sourceMappingURL=BaseResponse.js.map
