be1a588f022c7bc17b24f11a3a9190f500e6f53e
Xgt72 <xavier.georget@wildcodeschool.fr>
2019-10-08 14:36:51 +0200

    validation of tests in playerTeacherController.test.ts

--- packages/back/src/repositories/PlayerBuildingsRepository.ts
+++ packages/back/src/repositories/PlayerBuildingsRepository.ts
@@ -7,7 +7,7 @@ export class PlayerBuildingsRepository {
     }
 
     getOnePlayerBuildings(playerId: number) {
-        return getManager().getRepository(PlayerBuilding).createQueryBuilder("pb").where("pb.id = :id", {id: playerId}).getMany();
+        return getManager().getRepository(PlayerBuilding).createQueryBuilder("pb").where("pb.player_id = :id", {id: playerId}).getMany();
     }
 
     savePlayerBuilding(playerBuilding: PlayerBuilding) {
