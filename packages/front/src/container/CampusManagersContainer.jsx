9b14edba6a83c384e41cec5222c2f7addab023be
Xgt72 <xavier.georget@wildcodeschool.fr>
2019-11-13 09:34:49 +0100

    add of player menu

---
+++ packages/front/src/container/CampusManagersContainer.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from "react-router-dom";
+import {
+    Container, Row, Col, Button,
+} from 'reactstrap';
+
+class CampusManagersComponent extends Component {
+    render() {
+        return (
+            <>
+                <Container>
+                    <Row>
+                        <Col>
+                            <p>Campus Managers work</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
+}
+
+// const mapStateToProps = (state) => ({
+//     playerId: state.playerId,
+//     playerBuildings: state.playerBuildings,
+//   });
+
+//   const mapDispatchToProps = {
+//     addBuilding,
+//     updateIndicators,
+//   };
+
+//   const BuildingsContainer = connect(mapStateToProps, mapDispatchToProps)(BuildingsComponent);
+
+//   BuildingsComponent.propTypes = {
+//     playerId: PropTypes.number.isRequired,
+//     playerBuildings: PropTypes.array.isRequired,
+//     addBuilding: PropTypes.func.isRequired,
+//     updateIndicators: PropTypes.func.isRequired,
+//   };
+
+export default CampusManagersComponent;
