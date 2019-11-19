6ea1cc3dc3a0adcebcd888b2882773761f2a105c
Xgt72 <xavier.georget@wildcodeschool.fr>
2019-11-19 14:01:22 +0100

    add title in CampusManagersContainer

--- packages/front/src/components/DayActivities.jsx
+++ packages/front/src/components/DayActivities.jsx
@@ -12,9 +12,13 @@ class DayActivities extends Component {
             <div className="day">
                 <h3>{dayName}</h3>
                 <Row className="no-gutters mx-1">
-                    <Col style={dayActivities.morning != null ? {backgroundColor: `#${dayActivities.morning.color}`} : {}} className="addActivity morning d-flex justify-content-center align-items-center" onClick={(e) => action(e)}>
+                    <Col
+                        style={dayActivities.morning != null ? { backgroundColor: `#${dayActivities.morning.color}` } : {}}
+                        className="addActivity morning d-flex justify-content-center align-items-center"
+                        onClick={(e) => action(e)}
+                    >
                         {dayActivities.morning != null ?
-                            <p style={{color: "black"}}>{dayActivities.morning.name}</p> :
+                            <p style={{ color: "black" }}>{dayActivities.morning.name}</p> :
                             <p>Add activity</p>
                         }
                     </Col>
@@ -25,9 +29,13 @@ class DayActivities extends Component {
                     </Col>
                 </Row>
                 <Row className="no-gutters mx-1">
-                    <Col style={dayActivities.afternoon != null ? {backgroundColor: `#${dayActivities.afternoon.color}`} : {}} className="addActivity afternoon d-flex justify-content-center align-items-center" onClick={(e) => action(e)}>
+                    <Col
+                        style={dayActivities.afternoon != null ? { backgroundColor: `#${dayActivities.afternoon.color}` } : {}}
+                        className="addActivity afternoon d-flex justify-content-center align-items-center"
+                        onClick={(e) => action(e)}
+                    >
                         {dayActivities.afternoon != null ?
-                            <p style={{color: "black"}}>{dayActivities.afternoon.name}</p> :
+                            <p style={{ color: "black" }}>{dayActivities.afternoon.name}</p> :
                             <p>Add activity</p>
                         }
                     </Col>
